acf6ad4c2ec4e54d52f0e4453585a20e038e2c32
javasws <javasws@163.com>
2025-06-09 18:12:36 +0800

    【依赖问题】解决依赖

--- eslint.config.ts
+++ eslint.config.ts
@@ -28,6 +28,5 @@ export default defineConfigWithVueTs(
   {
     ...pluginPlaywright.configs["flat/recommended"],
     files: ["e2e/**/*.{test,spec}.{js,ts,jsx,tsx}"],
-  },
-  skipFormatting
+  }
 );
